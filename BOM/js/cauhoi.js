10169d99c345359d33608b1213e8820379fd4c41
Quang Dat <41713861+datnguyen3495@users.noreply.github.com>
2018-08-20 16:46:47 +0700

    ms

---
+++ BOM/js/cauhoi.js
@@ -0,0 +1,37 @@
+var cauhoi = [{
+	"cauhoi": "Từ đầu tiên trong 'Bảng chữ cái' là từ nào?",
+	"dapan1" :"a",
+	"dapan2" :"B",
+	"dapan3":"A",
+	"dapan4":"á",
+	"dapandung":"2"
+},{
+	"cauhoi":"Đền Taj Mahal thuộc thành phố nào Ấn Độ?",
+	"dapan1":"Mumbai",
+	"dapan2":"Agra",
+	"dapan3":"Delhi",
+	"dapan4":"Chennai",
+	"dapandung":"2"
+},{
+	"cauhoi":"Cây đàn balalaika độc đáo của nước Nga có mấy dây?",
+	"dapan1":"3",
+	"dapan2":"4",
+	"dapan3":"5",
+	"dapan4":"6",
+	"dapandung":"1",
+},{
+	'cauhoi':"'Phi dê' làm cho tóc như thế nào?",
+	'dapan1':"Nhọn",
+	'dapan2':"Thẳng",
+	'dapan3':"Vát",
+	'dapan4':"Xoăn",
+	'dapandung':"4"
+},{
+	'cauhoi':"Tác phẩm 'Đời thừa' của Nam Cao lần đầu tiên được đăng trên báo nào?",
+	'dapan1':"Ngày nay",
+	'dapan2':"Tiểu thuyết thứ 7",
+	'dapan3':"Nông cổ mím đàm",
+	'dapan4':"Phụ nữ tân văn",
+	'dapandung':"2"
+}
+];
---
+++ BOM/js/quiz.js
@@ -0,0 +1,52 @@
+
+var socauhoi = 0;
+var diem = 0;
+
+var tongcauhoi = cauhoi.length;
+
+var content = document.getElementById('content');
+var questionEl = document.getElementById('question');
+var  opt1 = document.getElementById('rep1');
+var  opt2 = document.getElementById('rep2');
+var  opt3 = document.getElementById('rep3');
+var  opt4 = document.getElementById('rep4');
+var win = document.getElementById('win');
+var lost = document.getElementById('lost');
+
+function back(){
+	window.location.href = "index.html";
+}
+
+function chaycauhoi(cauhoiIndex){
+	var q = cauhoi[cauhoiIndex];
+	questionEl.textContent = (cauhoiIndex + 1 ) + "." + q.cauhoi;
+	opt1.textContent =  q.dapan1;
+	opt2.textContent =  q.dapan2;
+	opt3.textContent =  q.dapan3;
+	opt4.textContent =  q.dapan4;
+footer();
+}
+
+chaycauhoi(socauhoi);
+function loadnext(x){
+	console.log(x.value);
+	if(x.value == cauhoi[socauhoi].dapandung){
+		diem +=1;
+	}
+	socauhoi++;
+	if(socauhoi == tongcauhoi && diem == 5){
+		warp.style.display = "none";
+		win.style.display = "";
+	}
+	if (socauhoi == tongcauhoi && diem <5) {
+		content.style.display = "none";
+		lost.style.display = "";
+		return;
+	}
+	chaycauhoi(socauhoi);
+	footer();
+}
+function footer(){
+	var diemcuoi = document.getElementById("footer");
+	diemcuoi.innerHTML = "Trình độ đạt đến :" + (socauhoi ) + "/" + tongcauhoi;
+}
