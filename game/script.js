297beaea2056925c0e2e7ab6dc97e9f037b711fb
Quang Dat <41713861+datnguyen3495@users.noreply.github.com>
2018-08-27 17:43:42 +0700

    1

--- game/script.js
+++ game/script.js
@@ -1,10 +1,10 @@
  const canvas = document.getElementById('my-canvas');
  const context= canvas.getContext('2d');
-canvas.width=560;
-canvas.height=350;
+ canvas.width=560;
+ canvas.height=350;
 
-const cvW = canvas.width;
-const cvH = canvas.height;
+ const cvW = canvas.width;
+ const cvH = canvas.height;
 let bX = 50;    //Vi tri ban dau
 let bY = cvH/2;
 
@@ -19,29 +19,29 @@ let pipe =[];
 
 
 function Component(img,x,y,width,height){
-this.img = img;
-this.x=x;
-this.y=y;
-this.width = width;
-this.height = height;
+	this.img = img;
+	this.x=x;
+	this.y=y;
+	this.width = width;
+	this.height = height;
 
 }
- Component.prototype.drawImg = function() {
- 	context.drawImage(this.img, this.x, this.y, this.width, this.height);
+Component.prototype.drawImg = function() {
+	context.drawImage(this.img, this.x, this.y, this.width, this.height);
  	// body...
  };
 
-const bg = new Image();
-const pipeSouth = new Image();
-const pipeNorth = new Image();
-const bird = new Image();
+ const bg = new Image();
+ const pipeSouth = new Image();
+ const pipeNorth = new Image();
+ const bird = new Image();
 
-bg.src = "img/background.jpeg";
-pipeSouth.src ='img/pipesouth.png';
-pipeNorth.src ='img/pipenorth.png';
-bird.src = 'img/bird.png';
+ bg.src = "img/background.jpeg";
+ pipeSouth.src ='img/pipesouth.png';
+ pipeNorth.src ='img/pipenorth.png';
+ bird.src = 'img/bird.png';
 
-
+window.addEventListener('click',handleEventKeyDown)
  window.addEventListener('keydown',handleEventKeyDown);
  function handleEventKeyDown(e){
  	if (bY<0) {
@@ -55,14 +55,31 @@ bird.src = 'img/bird.png';
  	background.drawImg();
  	const pipeN = new Component(pipeNorth,300,0,50,200);
  	const pipeS = new Component(pipeSouth,300,cvH -100 , 50, 100);
- 	pipeN.drawImg();
- 	pipeS.drawImg();
+
  	const birdF = new Component(bird,bX,bY,50,50);
  	birdF.drawImg();
  	bY += gravity; // roi tu do
  	//tao vat can theo thoi gian
  	sum ++; 
- 	
+ 	if(timeInterval(sum,timeRenderPipe)){
+ 		const randomHeight = Math.floor(Math.random()*(cvH-gap-50)+minHeightpipe);
+ 		pipe.push(new Component(pipeNorth,cvW,0,50,randomHeight));
+ 		pipe.push(new Component(pipeSouth,cvW,randomHeight+gap, 50,cvH-randomHeight-gap));
+ 	}
+ 	for(let i=0; i<pipe.length;i++){
+ 		pipe[i].drawImg();
+ 		pipe[i].x--;
+
+ 	}
+ 	function timeInterval(sum, time) {
+
+ 		if (sum / time % 1 === 0) {
+
+ 			return true;
+
+ 		} else return false;
+
+ 	}
  	requestAnimationFrame(update);
  }
  update();
