1c8e80085d92266f109b4fd10a5fea255de297ec
Quang Dat <41713861+datnguyen3495@users.noreply.github.com>
2018-08-28 17:17:29 +0700

    1

---
+++ memory card 1/js/script.js
@@ -0,0 +1,32 @@
+var cards = ["a1","a2","a3","a4","a5","a6","a7","a8","a9"];
+var current = null;
+
+
+function shuffle(array){
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    //Tạo random
+    while(0 !== currentIndex){
+        //Chọn phần tử
+        randomIndex=Math.floor(Math.random()* currentIndex);
+        currentIndex-=1;
+        //Cho nó random vs phần tử khác
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+$(document).ready(function(){
+    cards = cards.concat(cards); // nhân đôi lá bài
+    cards = shuffle(cards); // Tạo random các lá bài
+ //Chèn nội dung
+var html = "";
+for (var i = 0;i<cards.length;i++){
+    html+="<div class='gird'>"+
+     '<div class = "cards" data-name="' + cards[i] + '" onclick="flip(this)">' +
+     '<div class = "truoc"><img src = "./img/truoc.png"/></div>' +
+     '<div class = "sau"><img src = "./img/'+ cards[i] + '.jpg"/></div>' + '</div></div>';}
+$('.content').html(html);
+
+});
