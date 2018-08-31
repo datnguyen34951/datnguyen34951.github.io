193a9a96604907ebbd02bfe592016da61cd97aba
Quang Dat <41713861+datnguyen3495@users.noreply.github.com>
2018-08-31 21:26:44 +0700

    memocard 3 demo1

---
+++ memory card 3/js/script.js
@@ -0,0 +1,88 @@
+var cards = ["a1","a2","a3","a4","a5","a6","a7","a8","a9","a10","a11","a12"];
+var current = null;
+var count = 0;
+
+
+function shuffle(array){
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    //Tạo random
+    while(0 !== currentIndex){
+        //Chọn phần tử
+        randomIndex=Math.floor(Math.random()* currentIndex);
+        currentIndex-=1;
+        //Cho nó random vs phần tử khác
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+function flip(card) {
+    $(card).toggleClass('flipped');
+    
+    if(!current){
+        current = $(card);
+        $(current).css('pointer-events','none');
+        
+    }else{
+        
+        if (current.attr('data-name') != $(card).attr('data-name')){
+            $('.cards').css('pointer-events','none');
+            //Khác nhau
+            setTimeout(function(){
+            console.log('Khác nhau');
+            current.toggleClass('flipped');
+            $(card).toggleClass('flipped');
+            current = null;
+            $('.cards').css('pointer-events','auto');
+        }, 500);
+       
+
+        }else {
+            //Giống nhau
+            setTimeout(function(){
+            console.log('Giống nhau');
+            document.getElementById("giongnhau").play();
+            $(card).css('opacity','0');
+            current.css('opacity','0');
+            $(card).css('visibility','hidden');
+            current.css('visibility','hidden');
+            current = null;
+            count ++;
+            if(count == 9){
+                alert("win");
+            }
+           
+        },300);
+        }
+    }
+};
+
+$(document).ready(function(){
+    cards = cards.concat(cards); // nhân đôi lá bài
+    cards = shuffle(cards); // Tạo random các lá bài
+ //Chèn nội dung
+var html = "";
+for (var i = 0;i<cards.length;i++){
+    html+="<div class='gird'>"+
+     '<div class = "cards" data-name="' + cards[i] + '" onclick="flip(this)">' +
+     '<div class = "truoc"><img src = "./img/truoc.png"/></div>' +
+     '<div class = "sau"><img src = "./img/'+ cards[i] + '.jpg"/></div>' + '</div></div>';}
+$('.content').html(html);
+
+});
+function move(){
+    var elem = document.getElementById("time");
+    var width = 90;
+    var id = setInterval (frame,1000);
+    function frame(){
+        //Thua
+        if(width==0){
+            clearInterval(id);
+        } else {
+            width--;
+            elem.style.width = width + "%";
+        }
+    }
+}
+move();
