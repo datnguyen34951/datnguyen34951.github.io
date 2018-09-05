e3b10e995374a9e27dc1b54fe44a2a31a2d125cc
Quang Dat <41713861+datnguyen3495@users.noreply.github.com>
2018-09-05 21:21:43 +0700

    hoàn thành card3

--- memory card 3/js/script.js
+++ memory card 3/js/script.js
@@ -1,7 +1,22 @@
 var cards = ["a1","a2","a3","a4","a5","a6","a7","a8","a9","a10","a11","a12"];
 var current = null;
 var count = 0;
-
+const audio = document.getElementById("clickchuot");
+const audio1 = document.getElementById("giongnhau");
+var audio3 = document.getElementById('modau');
+$(document).ready(function(){
+    $(".start").click(function(){
+        $("#batdau").slideUp("slow");
+    });
+})
+$(document).ready(function(){
+    $(".start").click(function(){
+        $("#choi").fadeIn(2000);
+        document.getElementById('nhacnen').play();
+        document.getElementById('modau').pause();
+        move();
+    });
+})
 
 function shuffle(array){
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -17,13 +32,31 @@ function shuffle(array){
     }
     return array;
 }
+function move(){
+    var elem = document.getElementById("time");
+    var width = 90;
+    var id = setInterval (frame,1000);
+    function frame(){
+        //Cột thời gian
+        if(width==0){
+            clearInterval(id);
+            $("#choi").slideUp('slow');
+            $('#thatbai').slideDown('slow');
+            document.getElementById('nhacnen').pause();
+            document.getElementById('fail').play();
+        } else {
+            width--;
+            elem.style.width = width + "%";
+        }
+     }
+}
 function flip(card) {
     $(card).toggleClass('flipped');
-    
+    audio.load();
+    audio.play();
     if(!current){
         current = $(card);
-        $(current).css('pointer-events','none');
-        
+        $(current).css('pointer-events','none');      
     }else{
         
         if (current.attr('data-name') != $(card).attr('data-name')){
@@ -42,17 +75,26 @@ function flip(card) {
             //Giống nhau
             setTimeout(function(){
             console.log('Giống nhau');
-            document.getElementById("giongnhau").play();
-            $(card).css('opacity','0');
-            current.css('opacity','0');
+            audio1.load();
+            audio1.play();
+            $(card).css('transform','scale(0.5)');
+            current.css('transform','scale(0.5)');
             $(card).css('visibility','hidden');
             current.css('visibility','hidden');
             current = null;
             count ++;
-            if(count == 9){
-                alert("win");
+            //win
+            if(count == 12){
+            $('body').css("background-image","url(./img/win.gif)")
+            $('#snow').remove();
+            $("#thatbai").remove();
+            $('#choi').remove();
+            $("#win").fadeIn('slow');
+            audio.pause();
+            audio1.pause();
+            document.getElementById('nhacnen').pause();
+            document.getElementById('phaohoa').play();
             }
-           
         },300);
         }
     }
@@ -71,18 +113,14 @@ for (var i = 0;i<cards.length;i++){
 $('.content').html(html);
 
 });
-function move(){
-    var elem = document.getElementById("time");
-    var width = 90;
-    var id = setInterval (frame,1000);
-    function frame(){
-        //Thua
-        if(width==0){
-            clearInterval(id);
-        } else {
-            width--;
-            elem.style.width = width + "%";
-        }
-    }
+
+function autoplay(){
+    audio3.autoplay = true;
+    audio3.load();
 }
-move();
+autoplay();
+$(document).ready(function(){
+    $("#choilai").click(function(){
+        window.location.reload();
+    });
+})
